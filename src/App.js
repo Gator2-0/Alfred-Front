92788e550b3715319fb3ad578c6a918f56a50425
Gator2-0 <jejedech.63@gmail.com>
2024-08-13 12:05:18 +1000

    adding button to get API reposnse for diving

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [divingForecast, setDivingForecast] = useState("Diving data goes here...");
+
+  const handleDivingForecast = async () => {
+    try {
+      const response = await fetch("https://localhost:7116/DivingControllers/divingForecast");
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setDivingForecast(data);
+    } catch (error) {
+      console.error("Failed to fetch diving forecast:", error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleDivingForecast}>Get diving forecast</button>
+        {divingForecast && (
+        <div>
+          <pre>{JSON.stringify(divingForecast, null, 2)}</pre>
+        </div>
+      )}
       </header>
     </div>
   );
