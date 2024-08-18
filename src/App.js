377e4d978499192dd1ecf1c8c0abd8742f152271
Gator2-0 <jejedech.63@gmail.com>
2024-08-18 16:04:42 +1000

    adding text input for NLP service

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ import DivingCard from './components/divingCard';
 
 function App() {
   const [divingForecast, setDivingForecast] = useState(["Diving data goes here..."]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const response = await fetch("https://localhost:7089/Nlp/process-query",
+        {
+          method: 'POST', // Specify the method
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' // Specify the return content type
+          },
+          body: JSON.stringify(inputValue) 
+        });
+
+      if(!response.ok){
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setDivingForecast(data.data);
+      
+    } catch (error) {
+      console.error("Failed to fetch diving forecast:", error);
+    }
+  };
 
   const handleDivingForecast = async () => {
     try {
@@ -24,14 +56,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={handleDivingForecast}>Get diving forecast</button>
-        {divingForecast && (
-        <div>
-          {divingForecast.map((weatherData, index) =>(
-            <DivingCard weatherData={weatherData} key={index}/>
-          ))}
-        </div>
-      )}
+        <form onSubmit={handleSubmit}>
+              <label>
+                  Enter your query:
+                  <input
+                      type="text" 
+                      value={inputValue}
+                      onChange={handleInputChange}
+                      placeholder="Type your question here..."
+                  />
+              </label>
+              <button type="submit">Submit</button>
+          </form>
+          <button onClick={handleDivingForecast}>Get diving forecast</button>
+          {divingForecast && (
+            <div className='card-container'>
+              {divingForecast.map((weatherData, index) =>(
+                <DivingCard weatherData={weatherData} key={index}/>
+              ))}
+            </div>
+          )}
       </header>
     </div>
   );
